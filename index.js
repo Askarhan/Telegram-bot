cbe7cb54124355b586159779d3e211f59fcee744
Askarhan <drakula199821@gmail.com>
2025-09-02 17:59:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const app = express();
 app.use(express.json());
 
 const TOKEN = process.env.TOKEN;
-const bot = new TelegramBot(TOKEN);
-
-app.use(express.json());
-app.post(`/webhook`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
+const bot = new TelegramBot(TOKEN); 
 
 bot.on("message", (msg) => {
   bot.sendMessage(msg.chat.id, `Привет! Ты написал: ${msg.text}`);
+});
+
+app.post("/webhook", (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 app.get("/", (req, res) => {
   res.send("Сервер работает! 🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
