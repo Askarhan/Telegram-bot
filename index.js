ec9b36b99ac46400e3c6110e68233e4e3c0f8fbd
Askarhan <drakula199821@gmail.com>
2025-09-03 00:22:33 +0600

    Webhook index.js

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 10000;
 
 const TOKEN = '8370855958:AAHC8ry_PsUqso_jC2sAS9CnQnfURk1UW3w';
-const bot = new TelegramBot(TOKEN, { polling: true });
-
-app.use(express.json());
+const bot = new TelegramBot(TOKEN);
 
 app.get('/', (req, res) => {
-  res.send('Бот сервер работает!');
+  res.send('Сервер работает!');
 });
 
-app.post('8370855958:AAHC8ry_PsUqso_jC2sAS9CnQnfURk1UW3w', (req, res) => {
+app.post('/webhook', (req, res) => {
   const update = req.body;
-  console.log(update);
+
+  res.sendStatus(200);
 
   if (update.message) {
     const chatId = update.message.chat.id;
     const text = update.message.text || 'Пустое сообщение';
-    bot.sendMessage(chatId, `Ты написал: ${text}`);
+    bot.sendMessage(chatId, `Ты написал: ${text}`)
+       .catch(err => console.error(err));
   }
-
-  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
